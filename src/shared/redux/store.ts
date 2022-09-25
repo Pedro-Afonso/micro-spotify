baf140b48b52bb9034d14cf54de5ea07fc40b648
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-25 12:02:57 -0300

    feat: Add useAppDispatch and useAppSelector

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -1,6 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 
 import { shazamCoreApi } from './services/shazamCore'
+import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {
@@ -9,3 +10,9 @@ export const store = configureStore({
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(shazamCoreApi.middleware)
 })
+
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
