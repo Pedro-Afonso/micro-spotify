a97ac744df2814b5a8c168fa9fd3388f4c9c83bd
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-24 23:04:04 -0300

    feat: Add useGetTopChartsQuery to Discover page

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -1,3 +1,20 @@
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography
+} from '@mui/material'
+import { useEffect } from 'react'
+import { useGetTopChartsQuery } from '../shared/redux/services/shazamCore'
+
 export const Discover = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery()
+
+  if (isFetching) return <p>Carregando...</p>
+
+  if (error) return <p>Oops! Ocorreu algum erro...</p>
+
   return <div>Discover</div>
 }
