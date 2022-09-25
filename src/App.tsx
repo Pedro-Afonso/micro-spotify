e76fad80993fb9f15193a0d7af27b8e7dffe02c8
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-24 22:21:29 -0300

    feat: Add redux store to the project

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { AppRoutes } from './routes/AppRoutes'
 import { Sidebar } from './shared/components'
 import { AppSidebarProvider } from './shared/context/SidebarContext'
 
+import { Provider } from 'react-redux'
+import { store } from './shared/redux/store'
+
 export const App = () => {
   return (
     <BrowserRouter>
-      <AppSidebarProvider>
-        <Sidebar>
-          <AppRoutes />
-        </Sidebar>
-      </AppSidebarProvider>
+      <Provider store={store}>
+        <AppSidebarProvider>
+          <Sidebar>
+            <AppRoutes />
+          </Sidebar>
+        </AppSidebarProvider>
+      </Provider>
     </BrowserRouter>
   )
 }
