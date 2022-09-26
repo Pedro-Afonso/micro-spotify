efed6953d7366bb42aa129b0b049f5f7b6c584b6
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-26 10:39:32 -0300

    feat: Add player reducer to store

--- src/shared/redux/store.ts
+++ src/shared/redux/store.ts
@@ -2,10 +2,12 @@ import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
 
 import { shazamCoreApi } from './services/shazamCore'
 import { configureStore } from '@reduxjs/toolkit'
+import playerReducer from './slices/playerSlice'
 
 export const store = configureStore({
   reducer: {
-    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer
+    [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
+    player: playerReducer
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(shazamCoreApi.middleware)
