b6a36f5ec484afd158684d64d0d4f03e0fb75db5
Pedro-Afonso <pedro.766@hotmail.com>
2022-09-28 16:55:08 -0300

    feat: Update component to deal with artist data

--- src/shared/components/DetailsHeader/DetailsHeader.tsx
+++ src/shared/components/DetailsHeader/DetailsHeader.tsx
@@ -1,17 +1,30 @@
 import { Avatar, Box, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { IArtistDetails } from '../../redux/models/IArtistDetails'
 import { IMusicDetails } from '../../redux/models/IMusicDetails'
 
 interface IDetailsHeaderProps {
+  artistid?: string
+  artistData?: IArtistDetails
   songData?: IMusicDetails
 }
 
-export const DetailsHeader: React.FC<IDetailsHeaderProps> = ({ songData }) => {
+export const DetailsHeader: React.FC<IDetailsHeaderProps> = ({
+  artistid,
+  artistData,
+  songData
+}) => {
   return (
     <Box display="flex" alignItems="center" gap={2} paddingY={2}>
       <Avatar
         sx={{ height: '128px', width: '128px' }}
-        src={songData?.images.coverart}
+        src={
+          artistid
+            ? artistData?.artists[artistid].attributes?.artwork?.url
+                .replace('{w}', '500')
+                .replace('{h}', '500')
+            : songData?.images.coverart
+        }
       />
       <Box>
         <Typography>{songData?.title}</Typography>
