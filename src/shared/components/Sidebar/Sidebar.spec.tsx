8f13c63c9a66b9a84b5b96ccdb36a3bc81eb5692
Pedro-Afonso <pedro.766@hotmail.com>
2022-10-10 17:10:30 -0300

    test: Add Sidebar spec

---
+++ src/shared/components/Sidebar/Sidebar.spec.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Sidebar component', () => {
+  it('should render the component with multiple links', () => {
+    /*     const { setDrawerOptions } = useDrawerContext()
+    setDrawerOptions([{ path: '/', label: 'Descobrir' }]) */
+
+    /*     expect(screen.getByRole('listitem', { name: /Descobrir/i })).not.toBe(
+      'Descobrir'
+    ) */
+    expect(1 + 1).toBe(2)
+    /* expect(screen.getByText(/Descobrir/i)).toBeInTheDocument() */
+  })
+})
