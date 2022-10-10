31e76e8070f1c15f9e5927ab605b653fdbcf9573
Pedro-Afonso <pedro.766@hotmail.com>
2022-10-10 17:02:14 -0300

    refactor: Move useNavigate from ArtistCard to TopArtists

--- src/pages/TopArtists.tsx
+++ src/pages/TopArtists.tsx
@@ -1,13 +1,21 @@
 import { Box, Typography, Grow } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 import { ArtistCard } from '../shared/components/ArtistCard'
+import { IMusic } from '../shared/redux/models/IMusic'
 import { useGetTopChartsQuery } from '../shared/redux/services/shazamCore'
 
 export const TopArtists = () => {
+  const navigate = useNavigate()
+
   const { data, isFetching, error } = useGetTopChartsQuery()
 
   if (isFetching) return <p>Carregando...</p>
 
   if (error) return <p>Oops! Ocorreu algum erro...</p>
+
+  const handleNavigate = (song: IMusic) => {
+    navigate(`/artists/${song.artists[0].adamid}`)
+  }
 
   return (
     <Box>
@@ -15,9 +23,18 @@ export const TopArtists = () => {
         <Typography variant="h2">Top Artistas</Typography>
       </Box>
       <Grow in={!!data} timeout={2000}>
-        <Box display="flex" gap={4} flexWrap="wrap">
+        <Box
+          display="flex"
+          gap={2}
+          justifyContent="space-around"
+          flexWrap="wrap"
+        >
           {data?.map(song => (
-            <ArtistCard key={song.key} song={song} />
+            <ArtistCard
+              key={song.key}
+              song={song}
+              handleNavigate={handleNavigate}
+            />
           ))}
         </Box>
       </Grow>
--- src/shared/components/ArtistCard/ArtistCard.tsx
+++ src/shared/components/ArtistCard/ArtistCard.tsx
@@ -1,21 +1,32 @@
 import { Card, CardMedia, CardActionArea, Typography, Box } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
 import { IMusic } from '../../redux/models/IMusic'
 
 interface IArtistCardProps {
   song: IMusic
+  handleNavigate: (song: IMusic) => void
 }
 
-export const ArtistCard: React.FC<IArtistCardProps> = ({ song }) => {
-  const navigate = useNavigate()
+export const ArtistCard: React.FC<IArtistCardProps> = ({
+  song,
+  handleNavigate
+}) => {
+  const handleOnClick = () => {
+    handleNavigate(song)
+  }
+
   return (
-    <Card sx={{ width: 200 }}>
-      <CardActionArea
-        onClick={() => navigate(`/artists/${song.artists[0].adamid}`)}
-      >
+    <Card
+      sx={{
+        maxWidth: 230,
+        flexBasis: 200,
+        flexShrink: 1,
+        flexGrow: 1
+      }}
+    >
+      <CardActionArea onClick={handleOnClick}>
         <CardMedia
           component="img"
-          height="200"
+          height="max-content"
           image={song.images.background}
           alt={song.subtitle}
         />
