5a4356cf68514ce2aa9fda6904dc415eabf6aa4b
Pedro-Afonso <pedro.766@hotmail.com>
2022-10-10 17:09:44 -0300

    test: Add ArtistCard spec

---
+++ src/shared/components/ArtistCard/ArtistCard.spec.tsx
@@ -0,0 +1,41 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import { describe, expect, it, vi } from 'vitest'
+import { IMusic } from '../../redux/models/IMusic'
+import { ArtistCard } from './ArtistCard'
+
+describe('ArtistCard component', () => {
+  const props = {
+    song: {
+      images: { background: '/' },
+      subtitle: 'José'
+    } as IMusic,
+    handleNavigate: vi.fn()
+  }
+
+  it('should render an image element with alt and src attributes', () => {
+    render(<ArtistCard {...props} />)
+
+    const img = screen.getByRole('img')
+
+    expect(img).toHaveAttribute('src', props.song.images.background)
+    expect(img).toHaveAttribute('alt', props.song.subtitle)
+  })
+
+  it('should render the component with title element', () => {
+    render(<ArtistCard {...props} />)
+
+    const title = screen.getByRole('heading', { level: 3 })
+
+    expect(title).toHaveTextContent(props.song.subtitle)
+  })
+
+  it('should test handleNavigate function', () => {
+    render(<ArtistCard {...props} />)
+
+    const img = screen.getByRole('img')
+
+    fireEvent.click(img)
+
+    expect(props.handleNavigate).toHaveBeenCalled()
+  })
+})
