a9bbd39a4b72ad11d91c2051de97661366bd8dd0
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-28 11:23:55 -0300

    chore: Remove unnecessary attributes

--- src/App.tsx
+++ src/App.tsx
@@ -19,36 +19,15 @@ export const App = () => {
             <Sidebar>
               {/* Responsive Layout */}
               <Grid container>
-                <Grid
-                  item
-                  xs={12}
-                  sm={12}
-                  md={12}
-                  lg={8}
-                  order={{ xs: 1, lg: 1 }}
-                >
+                <Grid item xs={12} lg={8} order={{ xs: 1, lg: 1 }}>
                   <SearchBar />
                 </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  sm={12}
-                  md={12}
-                  lg={8}
-                  order={{ xs: 3, lg: 2 }}
-                >
+                <Grid item xs={12} lg={8} order={{ xs: 3, lg: 2 }}>
                   {/* Current page */}
                   <AppRoutes />
                   {/* /Current page */}
                 </Grid>
-                <Grid
-                  item
-                  xs={12}
-                  sm={12}
-                  md={12}
-                  lg={4}
-                  order={{ xs: 2, lg: 3 }}
-                >
+                <Grid item xs={12} lg={4} order={{ xs: 2, lg: 3 }}>
                   <TopPlay />
                 </Grid>
               </Grid>
